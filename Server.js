f07ce644dcd791f4d5fd65fd45c0eccfa86238c0
unknown <sarveshbhosale111@gmail.com>
2023-08-07 16:19:22 +0100

    Routes added

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require('dotenv').config();
 const mongoose = require("mongoose");
 const initateDB = require('./src/config/db.config');
-
+const users = require("./src/routers/api/v1/users/users.routes")
 //app config
 const app = express();
 const port = process.env.PORT || 5000;
@@ -66,7 +66,8 @@ const conn = mongoose.createConnection(uri, {
 );
 
 //routes
-app.get("/", (req, res) => res.status(200).send("Art Server Connected"))
+app.get("/", (req, res) => res.status(200).send("WanderScan Server Connected"))
+app.use("/user", users);
 
 //Listening
 app.listen(port, () => {
