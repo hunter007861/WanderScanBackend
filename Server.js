dae9d5ce60fcf53eb45481aa0608e3fe25c84a25
unknown <sarveshbhosale111@gmail.com>
2023-09-25 19:07:59 +0100

    minor changes

--- Server.js
+++ Server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const mongoose = require("mongoose");
-const initateDB = require('./src/config/db.config');
-const users = require("./src/routers/api/v1/users/users.routes")
+const initateDB = require("./src/config/db.config");
+const users = require("./src/routers/api/v1/users/users.routes");
 //app config
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,43 +15,13 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-var corsOptions = {
-    methods: "GET, PUT, POST, DELETE, PATCH",
-    credentials: true,
-    origin: "http://localhost:3000"
+const corsOptions = {
+  methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
+  credentials: true,
+  origin: ["http://localhost:3000", "http://192.168.0.115:3000", "*"],
+  optionsSuccessStatus: 200,
 };
 
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization,User-Authorization');
-    res.header('responseType', 'blob')
-    if (req.method == 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-});
-
-
-var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
-
-    next();
-}
-
-app.use(allowCrossDomain);
-app.options('*', cors())
 app.use(cors(corsOptions));
 
 //db config
@@ -60,16 +30,15 @@ initateDB();
 const uri = process.env.MONGO_URI;
 
 const conn = mongoose.createConnection(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
-);
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //routes
-app.get("/", (req, res) => res.status(200).send("WanderScan Server Connected"))
+app.get("/", (req, res) => res.status(200).send("WanderScan Server Connected"));
 app.use("/user", users);
 
 //Listening
-app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`);
+app.listen(port,'192.168.0.115', () => {
+  console.log(`Server is running on port: ${port}`);
 });
